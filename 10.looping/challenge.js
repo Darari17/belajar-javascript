671657c9f43cb804af6f16ba773484ac784e8392
farid <faridramadhan59@gmail.com>
2024-09-16 21:50:23 +0700

    TEST LOOPING

--- 10.looping/challenge.js
+++ 10.looping/challenge.js
@@ -11,3 +11,21 @@ for (let i = 1; i <= rows; i++) {
   }
   console.log(str);
 }
+
+// yang ini juga bisa
+
+let a = 5;
+
+for (let i = 0; i <= a; i++) {
+  let b = "";
+
+  for (let j = i; j < a; j++) {
+    b += " ";
+  }
+
+  for (let k = 0; k < 2 * i - 1; k++) {
+    b += "*";
+  }
+
+  console.log(b);
+}
